50565118fbce0005aacda80615c5b92ca9bd50c7
heesarang <heesarang@gmail.com>
2019-07-24 04:11:09 +0900

    store commit CHOI-3

--- Store/src/main/java/com/sk/ani/lineup/store/domain/model/AggregateRoot.java
+++
@@ -1,4 +0,0 @@
-package com.sk.ani.lineup.store.domain.model;
-
-public interface AggregateRoot {
-}
--- Store/src/main/java/com/sk/ani/lineup/store/domain/model/MenuType.java
+++
@@ -1,7 +0,0 @@
-package com.sk.ani.lineup.store.domain.model;
-
-public enum MenuType {
-	MainDish,
-	SideDish,
-	Beverage
-}
