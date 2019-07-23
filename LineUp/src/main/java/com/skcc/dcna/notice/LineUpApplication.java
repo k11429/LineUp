aa31a61297eab615c1afdcc91c374b3df1da79e1
warren98 <warren98@naver.com>
2019-07-23 15:40:04 +0900

    warren98

---
+++ LineUp/src/main/java/com/skcc/dcna/notice/LineUpApplication.java
@@ -0,0 +1,13 @@
+package com.skcc.dcna.notice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LineUpApplication.class, args);
+	}
+
+}
